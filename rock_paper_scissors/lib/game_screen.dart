331b981e2d55ec2a4611480ad1dfa79020d4ae5c
abdyevas <sabina.abdyeva@ufaz.az>
2024-01-01 23:25:36 +0400

    game screen created

---
+++ rock_paper_scissors/lib/game_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GameScreen();
+  }
+}
+
+class _GameScreen extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Rock'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Paper'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Scissors'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- rock_paper_scissors/lib/main.dart
+++ rock_paper_scissors/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:rock_paper_scissors/game_screen.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 67, 90, 13),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 12, 129, 202),
+);
 
 void main() {
   runApp(
     const MaterialApp(
-      
+      home: GameScreen(), 
     ),
   );
 }
