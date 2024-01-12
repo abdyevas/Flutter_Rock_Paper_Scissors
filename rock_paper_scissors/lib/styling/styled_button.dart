1e9cb668fca0dd75c5027ba517bf3e68504b7831
abdyevas <sabina.abdyeva@ufaz.az>
2024-01-12 23:49:14 +0400

    updates

---
+++ rock_paper_scissors/lib/styling/styled_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StyledButton extends StatelessWidget {
+  const StyledButton({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
