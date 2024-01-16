a3a632e70b6cb83e9c35ef107439d0db1a06504e
abdyevas <sabina.abdyeva@ufaz.az>
2024-01-16 23:24:49 +0400

    add a random choice

--- rock_paper_scissors/lib/widgets/button_element.dart
+++ rock_paper_scissors/lib/widgets/button_element.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:rock_paper_scissors/styling/styled_button.dart';
 
 class ButtonElement extends StatefulWidget {
@@ -17,7 +18,22 @@ class _ButtonElementState extends State<ButtonElement> {
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: MyStyledButton.filledTonal(),
-      onPressed: widget.element != null ? () {} : () {},
+      onPressed: widget.element != null 
+      ? () {
+        int playerChoice = 0;
+        if (widget.element == 'rock') {
+          playerChoice = 1;
+        } else if (widget.element == 'paper') {
+          playerChoice = 2;
+        } else if (widget.element == 'scissors') {
+          playerChoice = 3;
+        }
+        print(playerChoice);
+      } 
+      : () {
+        int opponentChoice = Random().nextInt(3) + 1;
+        print(opponentChoice);
+      },
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
